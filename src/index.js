e862c48ffd5cdb110b9d142f52967f0f25740e12
abubkar <abubakaraw8@gmail.com>
2024-09-13 11:16:13 +0500

    database connected

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import express from 'express';
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
 
 
 const app = express();
+
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 3001, () => {
+        console.log("Server started on port", process.env.PORT);
+    })
+})
+.catch((err) => {
+    console.log("Mongodb connection failed !!! ", err);
+})
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
